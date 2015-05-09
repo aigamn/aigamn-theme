ccb22b337baca4e88ddaae30776690db80e7464d
Pete <pete@Petes-MacBook-Pro.local>
2015-05-09 17:08:50 -0500

    Added some page stuff

--- themes/aiga-minnesota/index.php
+++ themes/aiga-minnesota/index.php
@@ -2,7 +2,11 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<div class='container'>
-		<h1><?php the_title(); ?></h1>
+		<h1>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h1>
 
 		<?php the_content() ?>
 	</div>

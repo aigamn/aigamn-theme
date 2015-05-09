15183d7e0e1e99384c84913ee181b4579e51fab1
Pete <pete@Petes-MacBook-Pro.local>
2015-05-09 15:16:06 -0500

    Added Disqus, fixed thumbnail reference

--- themes/aiga-minnesota/functions.php
+++ themes/aiga-minnesota/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-    add_theme_support( 'post-thumbnails', array( 'post', 'page' ));
+    add_theme_support( 'post-thumbnails');
 
     include 'functions-event.php';
     include 'functions-community.php';

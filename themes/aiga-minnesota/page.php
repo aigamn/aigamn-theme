b07c228b01a6f7a3d0f493ad08c59ba999d1d814
Pete <pete@Petes-MacBook-Pro.local>
2015-05-09 17:32:17 -0500

    Fixed the background issue

--- themes/aiga-minnesota/page.php
+++ themes/aiga-minnesota/page.php
@@ -1,44 +1,46 @@
 <?php include_once('header.php'); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-	<div class='background'> <!--temporary solution to show blue background at top of page -->
+	<div class='background'>
+		<?php the_post_thumbnail('large', array('class'=>'img-responsive')) ?>
+	</div>
 
-		<article class='container single'>
 
-			<header class='cta-header'>
+	<article class='container single'>
 
-				<h1><?php the_title() ?></h1>
-				
-				<div class='main-image visible-xs'>
-					<?php the_post_thumbnail('medium', array('class'=>'img-responsive')) ?>
-				</div>
+		<header class='cta-header'>
 
-				<a href='mailto:<?php echo get_the_author_meta( 'user_email' ); ?>' class='btn btn-default cta'>
-					Contact
-					<br>
-					<small>
-						<?php echo get_the_author_meta( 'user_email' ); ?>
-					</small>
-				</a>
-
-			</header>
-
-			<div class='col-md-10 col-md-offset-1'>
-
-				<div class='main-image hidden-xs'>
-					<?php the_post_thumbnail('large', array('class'=>'img-responsive')) ?>
-					<a href='#'>
-						<img src='images/pin-it.png' class='pin-it' alt='pin it place holder' />
-					</a>
-				</div>
-				<div class='main-text'>
-					<?php the_content() ?>
-				</div> <!-- end .main-text -->
+			<h1><?php the_title() ?></h1>
+			
+			<div class='main-image visible-xs'>
+				<?php the_post_thumbnail('medium', array('class'=>'img-responsive')) ?>
 			</div>
 
-		</article>
+			<a href='mailto:<?php echo get_the_author_meta( 'user_email' ); ?>' class='btn btn-default cta'>
+				Contact
+				<br>
+				<small>
+					<?php echo get_the_author_meta( 'user_email' ); ?>
+				</small>
+			</a>
 
-	</div>
+		</header>
+
+		<div class='col-md-10 col-md-offset-1'>
+
+			<div class='main-image hidden-xs'>
+				<?php the_post_thumbnail('large', array('class'=>'img-responsive')) ?>
+				<a href='#'>
+					<img src='images/pin-it.png' class='pin-it' alt='pin it place holder' />
+				</a>
+			</div>
+			<div class='main-text'>
+				<?php the_content() ?>
+			</div> <!-- end .main-text -->
+		</div>
+
+	</article>
+
 <?php endwhile; ?>
 
 <?php include_once('footer.php'); ?>

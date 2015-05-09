71a5780ae8961d3f7ec89ef475a5076274143755
Pete <pete@Petes-MacBook-Pro.local>
2015-05-09 16:00:03 -0500

    Added shortcode for getting username, added disqus notification

--- themes/aiga-minnesota/functions.php
+++ themes/aiga-minnesota/functions.php
@@ -7,4 +7,30 @@
     include 'functions-post-footer.php';
     include 'functions-sponsor.php';
 
+    // [get_name username="username"]
+    function get_name_func( $atts ) {
+        $a = shortcode_atts( array(
+            'username' => ''
+        ), $atts );
+
+        $username = $a['username'];
+        //die($username);
+        $user = get_user_by( 'login', $username );
+        
+
+        return $user->display_name;
+    }
+    add_shortcode( 'get_name', 'get_name_func' );
+    /*add_action( 'init', 'testfunc' );
+    function testfunc(){
+        $a = shortcode_atts( array(
+            'username' => ''
+        ), $atts );
+
+        $username = $a['username'];
+        $user = get_user_by( 'login', $username );
+        die($user);
+
+        return $user;
+    }*/
 ?>
